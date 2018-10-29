17f27476a33acb42c2e048799dbdaba182e720af
Austin Sun <sunaustin8@gmail.com>
2018-10-29 00:46:01 -0500

    calendar fix

--- app/src/main/java/com/example/health_companion_uiuc_mobile/PlanFragment.java
+++ app/src/main/java/com/example/health_companion_uiuc_mobile/PlanFragment.java
@@ -147,7 +147,7 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
                 */
                 EditText eventName = (EditText) mView.findViewById(R.id.nameEvent);
                 String nameOfEvent = eventName.getText().toString();
-                Toast.makeText(getActivity(), nameOfEvent + " has been successfully added to your calendar.",
+                //Toast.makeText(getActivity(), nameOfEvent + " has been successfully added to your calendar.",
                         Toast.LENGTH_LONG).show();
                 break;
         }
